c1a88958ddd3597ace8bb04e6401bfa10167bd67
GUPTA <pg2312@att.com>
2018-03-23 23:00:25 +0530

    implement logging

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,9 +1,19 @@
 from kafka import KafkaConsumer
 import json
 import requests
+import logging
+
 
 ### VizGems REST END point
 vizgems_rest_url = "http://135.25.224.231:8000/cgi-bin/vg_rest.cgi"
+
+### Logging 
+handler = logging.FileHandler('/export/home/swift/log/kafkaconsumer.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 ### Creating Instance KafKa Consumer
 cdp_receiver = KafkaConsumer('vizgems_in',
@@ -14,11 +24,13 @@ cdp_receiver = KafkaConsumer('vizgems_in',
 ### consuming Massages
 payload = dict()
 for message in cdp_receiver:
-   ### Formating received data in JSON format
-   payload = json.loads(message.value.replace("'", "\""))
+   ### Formating received data in JSON format   
+   payload = json.loads(message.value.replace("'", "\""))   
    try:
       ### Sending the request to VizGems app for Config the Services
       response = requests.post(vizgems_rest_url, data=payload)
+	  logger.info(payload + ' Response: '+response.json())
       #print(response.json())
-   except:
-      print("fail")
+   except Exception as e:      
+	  logger.error(e.message)
+
